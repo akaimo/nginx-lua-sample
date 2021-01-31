b676c531180e71bdbb9a63188a7fde2a38fb4b33
akaimo <kawashu0723@gmail.com>
2021-01-31 15:05:30 +0900

    add json response

--- container/api/main.go
+++ container/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -8,6 +9,29 @@ import (
 func main() {
 	http.HandleFunc("/hc", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ok")
+	})
+	http.HandleFunc("/api/json", func(w http.ResponseWriter, r *http.Request) {
+		v := struct {
+			Ok      bool `json:"ok"`
+			Message string `json:"message"`
+		}{
+			Ok:      true,
+			Message: "ok!",
+		}
+		j, _ := json.Marshal(v)
+		_, _ = w.Write(j)
+	})
+	http.HandleFunc("/api/error", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		v := struct {
+			Ok      bool `json:"ok"`
+			Message string `json:"message"`
+		}{
+			Ok:      false,
+			Message: "error!!!",
+		}
+		j, _ := json.Marshal(v)
+		_, _ = w.Write(j)
 	})
 	http.ListenAndServe(":8080", nil)
 }
